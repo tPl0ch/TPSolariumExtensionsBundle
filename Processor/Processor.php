36d573835177cffc4b92df66ce5e0025eab1f5c6
Thomas Ploch <thomas.ploch@nunnit.com>
2013-03-22 22:50:46 +0100

    Addec check for Traversable in Processor::processSave()

--- Processor/Processor.php
+++ Processor/Processor.php
@@ -177,6 +177,14 @@ class Processor
         /** @var \TP\SolariumExtensionsBundle\Metadata\PropertyMetadata $property */
         foreach ($classMetadata->propertyMetadata as $property) {
             if (true === $property->multi) {
+                $traversable = $property->getValue($object);
+
+                if (!is_array($traversable) && !$traversable instanceof \Traversable && !$traversable instanceof \stdClass) {
+                    $message = "Field '%s' is declared as multi field, but property value is not traversable.";
+
+                    throw new \InvalidArgumentException(sprintf($message, $property->name));
+                }
+                
                 foreach ($property->getValue($object) as $item) {
                     if ($property->propertyAccess === PropertyMetadata::TYPE_RAW) {
                         $value = $item;
